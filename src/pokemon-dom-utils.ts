18548ac7ef650431ace99ff65abe2f6108157179
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-08-30 14:04:47 +0200

    Can heal pokemon

--- src/pokemon-dom-utils.ts
+++ src/pokemon-dom-utils.ts
@@ -1,38 +1,130 @@
-import { Utils } from './utils.js';
-import { ValueWithTotal } from './models/pokemon.js';
+import { Utils } from "./utils.js";
+import { ValueWithTotal } from "./models/pokemon.js";
+
+export interface HealthElts {
+  healthValueTextElt: HTMLElement;
+  healthBarElt: HTMLElement;
+}
+
+export interface BodyElts {
+  healthElts: HealthElts;
+  healButtonElt: HTMLElement;
+}
+
+const lifeColors = {
+  low: "#ab3333",
+  medium: "#d4ca38",
+  high: "#4caf50"
+};
 
 export class PokemonDomUtils {
-  private static createPokemonCardHeader (cardElt: HTMLElement, pokemonImgUrl: string) {
-    const header = Utils.createNewElement("div", "pokemon-card-header", cardElt);
+  private static createPokemonCardHeader(
+    cardElt: HTMLElement,
+    pokemonImgUrl: string
+  ) {
+    const header = Utils.createNewElement(
+      "div",
+      "pokemon-card-header",
+      cardElt
+    );
     const img = Utils.createNewElement("div", "pokemon-card-img", header);
     img.style.backgroundImage = `url("${pokemonImgUrl}")`;
   }
-  
-  private static createPokemonBody (cardElt: HTMLElement, pokemonName: string, pokemonLife: ValueWithTotal) {
+
+  private static createPokemonBody(
+    cardElt: HTMLElement,
+    pokemonName: string,
+    pokemonLife: ValueWithTotal,
+    healFunction: () => void
+  ): BodyElts {
     const bodyElt = Utils.createNewElement("div", "pokemon-card-body", cardElt);
     Utils.createNewElement("h2", "pokemon-card-title", bodyElt, pokemonName);
-    PokemonDomUtils.createPokemonHealthInfo(bodyElt, pokemonLife);
-    PokemonDomUtils.createPokemonHealButton(bodyElt);
-  }
-  
-  private static createPokemonHealthInfo(bodyElt: HTMLElement, pokemonLife: ValueWithTotal) {
-    Utils.createNewElement("p", "pokemon-card-text", bodyElt, `HP: ${pokemonLife.current}`);
-    const healthbarContainer = Utils.createNewElement("div", "pokemon-health-bar-container", bodyElt);
-    const healthBar = Utils.createNewElement("div", "pokemon-health-bar", healthbarContainer);
-  
-    const percent = pokemonLife.current / pokemonLife.total * 100;
-    console.log(percent);
-  
-    healthBar.style.width = `${ percent }%`;
-  }
-  
-  private static createPokemonHealButton (bodyElt: HTMLElement) {
-    Utils.createNewElement("button", "button pokemon-card-button", bodyElt, "Heal Now");
+    const healthElts = PokemonDomUtils.createPokemonHealthInfo(
+      bodyElt,
+      pokemonLife
+    );
+    const healButtonElt = PokemonDomUtils.createPokemonHealButton(bodyElt, healFunction);
+    return {
+      healthElts,
+      healButtonElt,
+    };
   }
 
-  static createPokemonCard (pokemonCards: HTMLElement, pokemonName: string, pokemonImgUrl: string, pokemonLife: ValueWithTotal) {
-    const cardElt = Utils.createNewElement("div", 'pokemon-card', pokemonCards);
+  private static createPokemonHealthInfo(
+    bodyElt: HTMLElement,
+    pokemonLife: ValueWithTotal
+  ): HealthElts {
+    const healthTextElt = Utils.createNewElement(
+      "p",
+      "pokemon-card-text",
+      bodyElt,
+      `HP: `
+    );
+    const healthValueTextElt = Utils.createNewElement(
+      "span",
+      "pokemon-card-current-health-text",
+      healthTextElt,
+      pokemonLife.current.toString()
+    );
+    const healthbarContainer = Utils.createNewElement(
+      "div",
+      "pokemon-health-bar-container",
+      bodyElt
+    );
+    const healthBarElt = Utils.createNewElement(
+      "div",
+      "pokemon-health-bar",
+      healthbarContainer
+    );
+    const percent = (pokemonLife.current / pokemonLife.total) * 100;
+
+    PokemonDomUtils.changeHealthBarColorFromHealthPercent(percent, healthBarElt);
+
+    healthBarElt.style.width = `${percent}%`;
+    return {
+      healthValueTextElt,
+      healthBarElt,
+    };
+  }
+
+  static changeHealthBarColorFromHealthPercent(healthPercent: number, healthBarElt: HTMLElement) {
+    if (healthPercent < 30) {
+      healthBarElt.style.backgroundColor = lifeColors.low;
+    } else if (healthPercent < 70) {
+      healthBarElt.style.backgroundColor = lifeColors.medium; 
+    } else {
+      healthBarElt.style.backgroundColor = lifeColors.high; 
+    }
+  }
+
+  private static createPokemonHealButton(bodyElt: HTMLElement, healFunction: () => void) {
+    const button = Utils.createNewElement(
+      "button",
+      "button pokemon-card-button",
+      bodyElt,
+      "Heal"
+    );
+
+    button.addEventListener("click", healFunction, true);
+
+    return button;
+  }
+
+  static createPokemonCard(
+    pokemonCards: HTMLElement,
+    pokemonName: string,
+    pokemonImgUrl: string,
+    pokemonLife: ValueWithTotal,
+    healFunction: () => void,
+  ): BodyElts {
+    const cardElt = Utils.createNewElement("div", "pokemon-card", pokemonCards);
     PokemonDomUtils.createPokemonCardHeader(cardElt, pokemonImgUrl);
-    PokemonDomUtils.createPokemonBody(cardElt, pokemonName, pokemonLife);
+    const bodyElts = PokemonDomUtils.createPokemonBody(
+      cardElt,
+      pokemonName,
+      pokemonLife,
+      healFunction
+    );
+    return bodyElts;
   }
 }
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,5 +1,5 @@
-import { IPokemon, ValueWithTotal, PokemonGender } from './models/pokemon.js'; 
-import { PokemonDomUtils } from './pokemon-dom-utils.js'; 
+import { IPokemon, ValueWithTotal, PokemonGender } from "./models/pokemon.js";
+import { PokemonDomUtils, BodyElts } from "./pokemon-dom-utils.js";
 
 export class Pokemon implements IPokemon {
   imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.id}.png`;
@@ -8,19 +8,40 @@ export class Pokemon implements IPokemon {
     current: 25,
     total: 100,
   };
-  lifePointsElt?: HTMLElement | undefined;
-  lifeBarElt?: HTMLElement | undefined;
+
+  cardElts?: BodyElts;
 
   constructor(
     readonly id: number,
     readonly gender: PokemonGender,
-    readonly name: string,
+    readonly name: string
   ) {
     this.createHTMLElement();
   }
 
   createHTMLElement() {
-    const pokemonCards = document.querySelector(".pokemon-cards")! as HTMLElement;
-    PokemonDomUtils.createPokemonCard(pokemonCards, this.name, this.imgUrl, this.life);
+    const pokemonCards = document.querySelector(
+      ".pokemon-cards"
+    )! as HTMLElement;
+    this.cardElts = PokemonDomUtils.createPokemonCard(
+      pokemonCards,
+      this.name,
+      this.imgUrl,
+      this.life,
+      this.heal.bind(this)
+    );
+  }
+
+  heal() {
+    if (!this.cardElts) return;
+    this.life.current = this.life.total;
+    this.changeHealElementstoFull(this.life.current);
+  }
+
+  private changeHealElementstoFull(newHealthValue: number) {
+    if (!this.cardElts) return;
+    this.cardElts.healthElts.healthBarElt.style.width = "100%";
+    this.cardElts.healthElts.healthValueTextElt.textContent = newHealthValue.toString();
+    PokemonDomUtils.changeHealthBarColorFromHealthPercent(100, this.cardElts.healthElts.healthBarElt);
   }
 }
