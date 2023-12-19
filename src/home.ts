51ecafbe2a59f332d917bc2fc9cb2d3a5d91566e
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-12-19 15:51:43 +0100

    la fonction de création d'une card fonctionne

--- src/home.ts
+++ src/home.ts
@@ -1,67 +1,77 @@
-const cards = document.querySelector('.pokemon-cards');
+const cards = document.querySelector(".pokemon-cards");
 
 const pokemons = [
   {
     name: "Lucky",
-    picture: "https://placekitten.com/200/287"
+    picture: "https://placekitten.com/200/287",
   },
   {
     name: "Symba",
-    picture: "https://placekitten.com/200/139"
+    picture: "https://placekitten.com/200/139",
   },
   {
     name: "Léo",
-    picture: "https://placekitten.com/200/90"
+    picture: "https://placekitten.com/200/90",
   },
   {
     name: "Milo",
-    picture: "https://placekitten.com/200/194"
+    picture: "https://placekitten.com/200/194",
   },
   {
     name: "Charly",
-    picture: "https://placekitten.com/200/179"
-  }
+    picture: "https://placekitten.com/200/179",
+  },
 ];
 
-createCard("Charly", "https://placekitten.com/200/179");
+createCard(
+  "Charly",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+);
 
 function createCard(title: string, imageUrl: string) {
-
   if (cards === null) {
-    console.error('cards element should not be null');
+    console.error("cards element should not be null");
     return;
   }
 
-  // On crée un div avec la classe card
   const card = document.createElement("div");
-  card.classList.add("card");
+  card.classList.add("pokemon-card");
   cards.appendChild(card);
 
-  // On crée un div avec la classe card-header et on l'ajoute à la card
   const cardHeader = document.createElement("div");
-  cardHeader.classList.add("card-header");
+  cardHeader.classList.add("pokemon-card-header");
   card.appendChild(cardHeader);
 
-  // On crée un div avec la classe card-img et on l'ajoute à la cardHeader
   const cardImg = document.createElement("div");
   cardImg.style.backgroundImage = `url(${imageUrl})`;
-  cardImg.classList.add("card-img");
+  cardImg.classList.add("pokemon-card-img");
   cardHeader.appendChild(cardImg);
-  
-  
-  /**
-   * Étape 1: Créez la div cardBody, ajoutez la classe card-body et ajoutez-la à la carte
-   */
-  /**
-   * Étape 2
-   * Créez le h2 cardTitle, ajoutez la classe card-title,
-   * définissez le texte à l'intérieur de la balise sur le paramètre "title" de cette fonction
-   * et ajoutez-le à la cardBody
-   */
-  /**
-   * Étape 3
-   * Créez le bouton cardButton, ajoutez la classe card-button,
-   * définissez le texte à l'intérieur de la balise sur "Adopt Now"
-   * et ajoutez-le à la cardBody
-   */
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("pokemon-card-body");
+  card.appendChild(cardBody);
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("pokemon-card-title");
+  cardTitle.textContent = title;
+  cardBody.appendChild(cardTitle);
+
+  const cardText = document.createElement("p");
+  cardText.classList.add("pokemon-card-text");
+  cardText.textContent = 'HP: 100';
+  cardBody.appendChild(cardText);
+
+  const cardHealthBarContainer = document.createElement("div");
+  cardHealthBarContainer.classList.add("pokemon-health-bar-container");
+  cardBody.appendChild(cardHealthBarContainer);
+
+  const cardHealthBar = document.createElement("div");
+  cardHealthBar.classList.add("pokemon-health-bar");
+  cardHealthBarContainer.appendChild(cardHealthBar);
+
+  const cardBtn = document.createElement("button");
+  cardBtn.classList.add("pokemon-card-button");
+  cardBtn.classList.add("button");
+  cardBtn.textContent = 'Heal now';
+  cardBody.appendChild(cardBtn);
 }
